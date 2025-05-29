7d0c7ffa1ea083e4deb862bf16701eee56955888
Kshic <68031596+Olivex333@users.noreply.github.com>
2025-05-29 18:48:17 +0200

    Krótki opis zmian

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,21 +1,59 @@
+import re
 import tweepy
 import pandas as pd
+import os
+from dotenv import load_dotenv
+from textblob import TextBlob
+import matplotlib.pyplot as plt
 
-# Wstaw tutaj swoje klucze
-api_key = 'TWÓJ_API_KEY'
-api_secret = 'TWÓJ_API_SECRET'
-access_token = 'TWÓJ_ACCESS_TOKEN'
-access_token_secret = 'TWÓJ_ACCESS_TOKEN_SECRET'
+load_dotenv()
+BEARER_TOKEN = os.getenv('BEARER_TOKEN')
+client = tweepy.Client(bearer_token=BEARER_TOKEN)
 
-# Autoryzacja
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+response = client.search_recent_tweets(
+    query="#Nike lang:en",
+    max_results=20,
+    tweet_fields=['text']
+)
 
-# Pobierz 100 tweetów z wybranym hashtagiem (np. #Nike)
-tweets = tweepy.Cursor(api.search_tweets, q="#Nike", lang="en", tweet_mode='extended').items(100)
-data = [tweet.full_text for tweet in tweets]
+if response.data is None:
+    print("Nie znaleziono tweetów. Zmień zapytanie lub spróbuj później.")
+    exit()
 
-# Zapisz do DataFrame
+data = [tweet.text for tweet in response.data]
 df = pd.DataFrame(data, columns=['tweet'])
-df.to_csv('tweets.csv', index=False)
-print("Pobrano i zapisano tweety.")
+
+def clean_tweet(text):
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"@\w+", "", text)
+    text = re.sub(r"#\w+", "", text)
+    text = re.sub(r"[^A-Za-z\s]", "", text)
+    text = text.lower()
+    return text.strip()
+
+df['cleaned'] = df['tweet'].apply(clean_tweet)
+
+def get_sentiment(text):
+    polarity = TextBlob(text).sentiment.polarity
+    if polarity > 0.1:
+        return 'Positive'
+    elif polarity < -0.1:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
+df['sentiment'] = df['cleaned'].apply(get_sentiment)
+df.to_csv('tweets_sentiment.csv', index=False)
+print("Dodano analizę sentymentu i zapisano w tweets_sentiment.csv.")
+
+sentiment_counts = df['sentiment'].value_counts()
+labels = sentiment_counts.index
+sizes = sentiment_counts.values
+
+plt.figure(figsize=(6,6))
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140, colors=['green', 'red', 'gray'])
+plt.title('Rozkład sentymentów dla #Nike')
+plt.show()
+
+
+
